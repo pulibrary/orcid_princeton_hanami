320a96de1cd580a77cc91ebcd1c85b032dca879c
Carolyn Cole <cac9@princeton.edu>
2025-06-24 13:53:43 -0400

    Adding people_sift_row.rb as a Hanami Struct original code from https://github.com/pulibrary/orcid_princeton/blob/main/app/models/people_soft_row.rb

---
+++ app/structs/people_soft_row.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module OrcidPrinceton
+  module Structs
+    # ruby object for all the information we need to generate a row of the peoplesoft report
+    #  Remember that dry structs do not allow for mutation so there are no setters only getters
+    class PeopleSoftRow < OrcidPrinceton::DB::Struct
+      attribute :university_id, Types::String
+      attribute :row_type, Types::String.default('ORC')
+      attribute :netid, Types::String
+      attribute :full_name, Types::String
+      attribute :effective_from, Types::String
+      attribute :effective_until, Types::String
+
+      class << self
+        def new_from_user(user)
+          # TODO: we need the user model for this code
+          # PeopleSoftRow.new( university_id: user.university_id,
+          #                    netid: user.uid,
+          #                    full_name: user.display_name,
+          #                    orcid: user.orcid,
+          #                    effective_from: user.valid_token.created_at.strftime("%m/%d/%Y")
+          #                    effective_until: user.valid_token.expiration.strftime("%m/%d/%Y")
+        end
+      end
+    end
+  end
+end
