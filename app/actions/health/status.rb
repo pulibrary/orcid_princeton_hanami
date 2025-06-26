0202dff7b1a15b7cbdc3cdbc26646b9512c7a4e7
Sean Collins <sean@cllns.com>
2025-06-25 18:56:46 -0600

    Refactor to API status check to action

--- app/actions/health/status.rb
+++ app/actions/health/status.rb
@@ -5,15 +5,19 @@ module OrcidPrinceton
     module Health
       # handles the /health route
       class Status < OrcidPrinceton::Action
-        include Deps['views.health.status']
+        include Deps['operations.orcid_api_status']
 
         format :html, :json
 
         before :set_format_for_json_extension
 
         def handle(_request, response)
+          current_status = retrieve_orcid_id_status
+
           if response.format == :json
-            response.body = status.json_status.to_json
+            response.body = current_status.to_json
+          else
+            response[:current_status] = current_status
           end
         end
 
@@ -22,6 +26,15 @@ module OrcidPrinceton
         def set_format_for_json_extension(request, response)
           if request.path.end_with?('.json')
             response.format = :json
+          end
+        end
+
+        def retrieve_orcid_id_status
+          case orcid_api_status.call
+          in Dry::Monads::Result::Success(_orcid_status)
+            { status: 'OK', results: [{ name: 'ORCID', message: '', status: 'OK' }] }
+          in Dry::Monads::Result::Failure[:invalid, error]
+            { status: 'ERROR', results: [{ name: 'ORCID', message: error, status: 'ERROR' }] }
           end
         end
       end
