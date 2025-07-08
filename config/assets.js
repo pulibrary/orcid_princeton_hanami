0e3587941e0291a2c8bdac2fe50ff855af9e8939
carolyncole <1599081+carolyncole@users.noreply.github.com>
2025-07-08 14:46:16 -0400

    Added LUX header and footer (#12)
    Integrated Vue

--- config/assets.js
+++ config/assets.js
@@ -1,16 +1,19 @@
 import * as assets from 'hanami-assets';
 
-await assets.run();
+// await assets.run();
 
 // To provide additional esbuild (https://esbuild.github.io) options, use the following:
 //
 // Read more at: https://guides.hanamirb.org/assets/customization/
 //
-// await assets.run({
-//   esbuildOptionsFn: (args, esbuildOptions) => {
-//     // Add to esbuildOptions here. Use `args.watch` as a condition for different options for
-//     // compile vs watch.
-//
-//     return esbuildOptions;
-//   }
-// });
+await assets.run({
+  esbuildOptionsFn: (args, esbuildOptions) => {
+    // Add to esbuildOptions here. Use `args.watch` as a condition for different options for
+    // compile vs watch.
+    if (typeof esbuildOptions.alias == 'undefined') {
+      esbuildOptions.alias = {};
+    }
+    esbuildOptions.alias.vue = 'vue/dist/vue.esm-bundler.js';
+    return esbuildOptions;
+  },
+});
