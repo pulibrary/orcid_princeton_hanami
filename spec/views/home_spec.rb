297fde089a9058cd764fd84358a714e22eecc944
Carolyn Cole <cac9@princeton.edu>
2025-07-11 11:14:18 -0400

    Get main back up to 100% test coverage

--- spec/views/home_spec.rb
+++ spec/views/home_spec.rb
@@ -25,4 +25,24 @@ RSpec.describe OrcidPrinceton::Views::Home::Show do
   it 'exposes stale_version as empty string' do
     expect(rendered[:stale_version].value).to eq('')
   end
+
+  context 'the version is stale and is not released' do
+    let(:version_hash) do
+      { stale: true, sha: 'sha', branch: 'v0.8.0', version: '10 December 2021', tagged_release: false }
+    end
+
+    it 'exposes code_version hash' do
+      expect(rendered[:code_version].value).to eq(version_hash)
+    end
+
+    it 'exposes code_version_name' do
+      expect(rendered[:code_version_name].value).to(
+        eq('v0.8.0')
+      )
+    end
+
+    it 'exposes stale_version as empty string' do
+      expect(rendered[:stale_version].value).to eq('(stale)')
+    end
+  end
 end
