eefd6257158177ae57f0d6bd243b318e02a71015
carolyncole <1599081+carolyncole@users.noreply.github.com>
2025-07-11 10:40:22 -0400

    Adding capistrano tasks for deploying the hanami app (#16)

---
+++ config/deploy.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.19.2'
+
+set :application, 'orcid_princeton'
+set :repo_url, 'https://github.com/pulibrary/orcid_princeton_hanami.git'
+
+set :linked_dirs, %w[log public/assets node_modules]
+
+# Default branch is :main
+set :branch, ENV['BRANCH'] || 'main'
+
+set :deploy_to, '/opt/orcid_princeton'
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+# you can run on s single host with `cap --hosts=orcid-staging1.princeton.edu staging`
+
+namespace :hanami do
+  desc 'Marks the server(s) to be added back to the loadbalancer'
+  task :asset_compile do
+    on roles(:app) do
+      within release_path do
+        execute 'yarn', 'install'
+        execute 'bundle', 'exec hanami assets compile'
+      end
+    end
+  end
+end
+
+before 'deploy:publishing', 'hanami:asset_compile'
