3520e0f34853a6c372dfc577b82f7e9bee44351b
carolyncole <1599081+carolyncole@users.noreply.github.com>
2025-07-21 12:51:18 -0400

    Remove session/new action (#25)
    After integrating ORCI I realized if I configured Omniauth/Warden with get I could just forward auth requests for cas
    to auth/new and warden woouls grb them.  No need for my convoluted cas code in session/new
    
    fixes #24

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ module OrcidPrinceton
     root to: 'home.show'
     get '/health', to: 'health.status', as: :health
     get '/health.json', to: 'health.status'
-    get '/session/new', to: 'session.new', as: 'user_login'
     get '/auth/cas/callback', to: 'session.create', as: :auth_callback
     post '/auth/cas/callback', to: 'session.create'
     get '/admin/orcid_report', to: 'admin.orcid_report', as: :admin_report
