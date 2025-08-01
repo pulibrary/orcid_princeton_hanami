9f576b68489d99810b55f2b0f92f81e7b35e3913
Carolyn Cole <cac9@princeton.edu>
2025-08-01 14:11:25 -0400

    Changing the user profile page to display the correct ORCID profile link
    fixes #68

---
+++ spec/views/user_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+RSpec.describe OrcidPrinceton::Views::User::Show do
+  let(:view) { described_class.new }
+  let(:rom_user) { Factory[:user] }
+  let(:user) { OrcidPrinceton::Repos::UserRepo.new.get(rom_user.id) }
+  let(:version_hash) do
+    { stale: false, sha: 'sha', branch: 'v0.8.0', version: '10 December 2021', tagged_release: true }
+  end
+
+  # create a context that can handle the flash message by setting the request
+  # TODO:  This seems really hard.  I wonder if there is a better way...
+  let(:rack_request) { Rack::MockRequest.env_for('http://example.com/') }
+  let(:request) { Hanami::Action::Request.new(env: rack_request, params: {}, session_enabled: true) }
+  let(:default_context) { OrcidPrinceton::Views::Home::Show.config.values[:default_context] }
+  let(:context) do
+    Hanami::View::Context.new(request:, inflector: default_context.inflector,
+                              assets: default_context.assets, settings: default_context.settings,
+                              routes: default_context.routes)
+  end
+
+  subject(:rendered) { view.call(user: user, current_user: user, code_version: version_hash, context:) }
+
+  it 'exposes orcid_url' do
+    expect(rendered[:orcid_url].value).to eq('https://sandbox.orcid.org/')
+  end
+
+  context 'not the sandbox' do
+    it 'exposes orcid_url' do
+      allow(default_context.settings).to receive(:orcid_sandbox).and_return(false)
+      expect(rendered[:orcid_url].value).to eq('https://orcid.org/')
+    end
+  end
+end
