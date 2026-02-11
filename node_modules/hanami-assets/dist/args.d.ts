b6fc4b77b3944326be53a05855fa6cd3256ccd76
carolyncole <1599081+carolyncole@users.noreply.github.com>
2026-02-11 14:59:56 -0500

    Getting devbox working with lando and local tests (#213)

--- node_modules/hanami-assets/dist/args.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Args {
-    path: string;
-    dest: string;
-    watch: Boolean;
-    sri: string[] | null;
-}
-export declare const parseArgs: (args: string[]) => Args;
--- node_modules/hanami-assets/dist/esbuild.d.ts
+++
@@ -1,7 +0,0 @@
-import { BuildOptions, Plugin } from "esbuild";
-import { Args } from "./args.js";
-export interface EsbuildOptions extends Partial<BuildOptions> {
-    plugins: Plugin[];
-}
-export declare const buildOptions: (root: string, args: Args) => EsbuildOptions;
-export declare const watchOptions: (root: string, args: Args) => EsbuildOptions;
